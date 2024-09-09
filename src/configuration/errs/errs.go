c2c534aeb0f10d3d65be320d11df038bad416dd0
Marcus <marcus-v@outlook.com>
2024-09-09 20:38:16 -0300

    test: added testing to errs

---
+++ src/configuration/errs/errs.go
@@ -0,0 +1,76 @@
+package errs
+
+import (
+	"net/http"
+)
+
+// Errs takes message: the error description, err: stacktrace
+type Errs struct {
+	Message string    `json:"message"`
+	Err     string    `json:"error"`
+	Code    int64     `json:"code"`
+	Causes  []*Causes `json:"causes"`
+}
+
+func (e Errs) Error() string {
+	return e.Message
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func NewErrs(message, err string, code int64, causes []*Causes) *Errs {
+	return &Errs{
+		message,
+		err,
+		code,
+		causes,
+	}
+}
+
+func NewBadRequestError(message string) *Errs {
+	return &Errs{
+		message,
+		"bad_request",
+		http.StatusBadRequest,
+		nil,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []*Causes) *Errs {
+	return &Errs{
+		message,
+		"bad_request",
+		http.StatusBadRequest,
+		causes,
+	}
+}
+
+func NewInternalServerError(message string) *Errs {
+	return &Errs{
+		message,
+		"internal_server_error",
+		http.StatusInternalServerError,
+		nil,
+	}
+}
+
+func NewNotFoundError(message string) *Errs {
+	return &Errs{
+		message,
+		"not_found",
+		http.StatusNotFound,
+		nil,
+	}
+}
+
+func NewForbiddenError(message string) *Errs {
+	return &Errs{
+		message,
+		"forbidden",
+		http.StatusForbidden,
+		nil,
+	}
+}
---
+++ src/configuration/errs/errs_test.go
@@ -0,0 +1,29 @@
+package errs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewErrs(t *testing.T) {
+	causes := []*Causes{
+		{Field: "field1", Message: "error message 1"},
+		{Field: "field2", Message: "error message 2"},
+	}
+	err := NewErrs("error message", "some_error", 123, causes)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Code, int64(123))
+	assert.Equal(t, len(causes), 2)
+	assert.Equal(t, err.Err, "some_error")
+
+	message := err.Error()
+	assert.Equal(t, message, "error message")
+}
+
+func TestNewBadRequestError(t *testing.T) {
+	err := NewBadRequestError("bad request")
+	assert.Equal(t, err.Code, int64(400))
+	assert.Equal(t, err.Message, "bad request")
+}
