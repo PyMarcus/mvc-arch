9eb7514c566d424d8c1eb9711d8d8f20957cd1b0
Marcus <marcus-v@outlook.com>
2024-09-10 21:19:47 -0300

    feat(model): Added validations

--- src/model/request/user_request.go
+++ src/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Age      int    `json:"age"`
+	Name     string `json:"name" binding:"required,min=3,max=100"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=4,containsany=!@#$%"`
+	Age      int    `json:"age" binding:"required,min=0,max=125,numeric"`
 }
